291a827fa1896f52364e1d902623884f881b0a3f
schwa1997 <huimin.chen@studenti.unipd.it>
2024-11-15 21:48:35 +0100

    add new blog

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -54,7 +54,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/ai.jpg',
     navbar: {
       title: 'CHANGE陈记',
       logo: {
